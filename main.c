2d3175f197fbdd153b74e8e6f66cd26c205754c5
ImGabreuw <imgabreuw.dev@gmail.com>
2025-03-16 12:52:08 -0300

    feat: add file reading functionality and implement NFA processing

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 // Estrutura para representar um nó da lista de adjacência
 struct node
@@ -36,6 +37,39 @@ int nfa(node **graph, int current, char *input, int *accept, int start)
         temp = temp->next;
     }
     return 0;
+}
+
+const int MAX_LINE_LENGTH = 100;
+
+void read_line(FILE *file, int line_index, const char *mask, ...)
+{
+    if (file == NULL)
+    {
+        printf("Erro ao ler o arquivo.\n");
+        return;
+    }
+
+    fseek(file, 0, SEEK_SET);
+
+    char buffer[MAX_LINE_LENGTH];
+    int current_line = 1;
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        if (current_line == line_index)
+        {
+            size_t len = strlen(buffer);
+            if (len > 0 && buffer[len - 1] == '\n')
+                buffer[len - 1] = '\0';
+
+            va_list args;
+            va_start(args, mask);
+            vsscanf(buffer, mask, args);
+            va_end(args);
+            return;
+        }
+        current_line++;
+    }
+    printf("O arquivo tem menos de %d linhas.\n", line_index);
 }
 
 void interative_mode(void)
@@ -90,8 +124,72 @@ void interative_mode(void)
     }
 }
 
+void file_mode(void)
+{
+    FILE *file = fopen("in.txt", "r");
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    int i;
+
+    // Leitura do alfabeto
+    char alfabeto[50];
+    read_line(file, 1, "%s", alfabeto);
+
+    // Número de estados
+    int n;
+    read_line(file, 2, "%d", &n);
+    node *graph[n + 1];
+
+    for (i = 0; i <= n; i++)
+        graph[i] = NULL;
+
+    int accept[n + 1];
+    memset(accept, 0, sizeof(accept));
+
+    // Número de estados finais
+    int num_finais;
+    read_line(file, 3, "%d", &num_finais);
+
+    for (i = 1; i <= num_finais; i++)
+    {
+        int final_state;
+        read_line(file, 3 + i, "%d", &final_state);
+        accept[final_state] = 1;
+    }
+
+    // Número de transições
+    int num_transicoes;
+    read_line(file, 5, "%d", &num_transicoes);
+    for (i = 1; i <= num_transicoes; i++)
+    {
+        int origem, destino;
+        char simbolo;
+        read_line(file, 5 + i, "%d %c %d", &origem, &simbolo, &destino);
+        graph[origem] = push(graph[origem], simbolo, destino);
+    }
+
+    // Número de palavras a serem testadas
+    int num_palavras;
+    read_line(file, 10, "%d", &num_palavras);
+    for (i = 1; i <= num_palavras; i++)
+    {
+        char palavra[MAX_LINE_LENGTH];
+        read_line(file, 10 + i, "%s", &palavra);
+
+        if (nfa(graph, 0, palavra, accept, 0))
+            printf("%d: %s OK\n", i, palavra);
+        else
+            printf("%d: %s not OK\n", i, palavra);
+    }
+}
+
 int main(void)
 {
-    interative_mode();
+    // interative_mode();
+    file_mode();
     return EXIT_SUCCESS;
 }
